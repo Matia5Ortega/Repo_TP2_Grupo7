5f390ce94d4019412ee8682a0f05020e4942d4b2
LUCAS <LUCAS@DESKTOP-AEI65FV>
2023-06-16 20:56:30 -0300

    Se modifico las clases de Sucursal de Imp y controller

--- Tp2_Grupo7/src/main/java/ar/edu/unju/fi/service/imp/SucursalServiceImp.java
+++ Tp2_Grupo7/src/main/java/ar/edu/unju/fi/service/imp/SucursalServiceImp.java
@@ -7,40 +7,49 @@ import ar.edu.unju.fi.service.ISucursalService;
 
 public class SucursalServiceImp implements ISucursalService {
 
-	@Override
-	public List<Sucursal> getLista() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private List<Sucursal> listaSucursales;
+    private Sucursal sucursal;
 
-	@Override
-	public void guardar(Sucursal sucursal) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public List<Sucursal> getLista() {
+        return listaSucursales;
+    }
 
-	@Override
-	public Sucursal getBy(int Id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void guardar(Sucursal sucursal) {
+        listaSucursales.add(sucursal);
+    }
 
-	@Override
-	public void modificar(Sucursal sucursal) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public Sucursal getBy(int id) {
+        for (Sucursal s : listaSucursales) {
+            if (s.getId() == id) {
+                return s;
+            }
+        }
+        return null;
+    }
 
-	@Override
-	public void eliminar(Sucursal sucursal) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void modificar(Sucursal sucursal) {
+        Sucursal sucursalExistente = getBy(sucursal.getId());
+        if (sucursalExistente != null) {
+            sucursalExistente.setNombre(sucursal.getNombre());
+            sucursalExistente.setDireccion(sucursal.getDireccion());
+            sucursalExistente.setProvincia(sucursal.getProvincia());
+            sucursalExistente.setFechaInicio(sucursal.getFechaInicio());
+            sucursalExistente.setEmail(sucursal.getEmail());
+            sucursalExistente.setTelefono(sucursal.getTelefono());
+        }
+    }
 
-	@Override
-	public Sucursal getSucursal() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public void eliminar(Sucursal sucursal) {
+        listaSucursales.remove(sucursal);
+    }
 
+    @Override
+    public Sucursal getSucursal() {
+        return sucursal;
+    }
 }
