f8cf402d38bc0d708457ba52e234d8ca11adb66e
LUCAS <LUCAS@DESKTOP-AEI65FV>
2023-06-16 20:33:54 -0300

    Se creo las clases service para Sucursal

---
+++ Tp2_Grupo7/src/main/java/ar/edu/unju/fi/service/imp/SucursalServiceImp.java
@@ -0,0 +1,46 @@
+package ar.edu.unju.fi.service.imp;
+
+import java.util.List;
+
+import ar.edu.unju.fi.model.Sucursal;
+import ar.edu.unju.fi.service.ISucursalService;
+
+public class SucursalServiceImp implements ISucursalService {
+
+	@Override
+	public List<Sucursal> getLista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void guardar(Sucursal sucursal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Sucursal getBy(int Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void modificar(Sucursal sucursal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eliminar(Sucursal sucursal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Sucursal getSucursal() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
