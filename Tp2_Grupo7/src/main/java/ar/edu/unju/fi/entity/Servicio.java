7003240e777cd62104b8f42a4d87c4321bb19dcc
dariofunes <32348674+dariofunes@users.noreply.github.com>
2023-07-02 02:27:04 -0300

    Update Servicio.java

--- Tp2_Grupo7/src/main/java/ar/edu/unju/fi/entity/Servicio.java
+++ Tp2_Grupo7/src/main/java/ar/edu/unju/fi/entity/Servicio.java
@@ -3,17 +3,23 @@ package ar.edu.unju.fi.entity;
 import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @Component
+@Entity
+@Table(name="servicios")
 public class Servicio {
 	
-	@NotNull(message="Debe ingresar un número identificador.")
-	@Positive(message="El identificador debe ser un número mayor a cero.")
-	private int id;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
 	
 	@NotEmpty(message="Debe ingresar el nombre del paseador.")
 	@Size(min=3, max=30, message="El nombre del paseador debe tener entre 3 y 30 caracteres.")
@@ -23,21 +29,24 @@ public class Servicio {
 	@DateTimeFormat(pattern="dd/MM/yy HH:mm")
 	private LocalDateTime fechaHoraPaseo;
 	
+	private boolean estado;
+	
 	public Servicio() {
 		
 	}
 
-	public Servicio(int id, String nombrePaseador, LocalDateTime fechaHoraPaseo) {
+	public Servicio(long id, String nombrePaseador, LocalDateTime fechaHoraPaseo, boolean estado) {
 		this.id = id;
 		this.nombrePaseador = nombrePaseador;
 		this.fechaHoraPaseo = fechaHoraPaseo;
+		this.estado = estado;
 	}
 
-	public int getId() {
+	public long getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -55,6 +64,14 @@ public class Servicio {
 
 	public void setFechaHoraPaseo(LocalDateTime fechaHoraPaseo) {
 		this.fechaHoraPaseo = fechaHoraPaseo;
+	}
+
+	public boolean isEstado() {
+		return estado;
+	}
+
+	public void setEstado(boolean estado) {
+		this.estado = estado;
 	}
 	
 }
