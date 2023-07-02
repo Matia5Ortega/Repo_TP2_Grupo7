663c1ba6fa16c4b4891c8a5d30e397c4a38a9d07
LUCAS <LUCAS@DESKTOP-AEI65FV>
2023-07-01 22:46:22 -0300

    Se corrigieron errores

--- Tp2_Grupo7/src/main/java/ar/edu/unju/fi/repository/SucursalRepository.java
+++ Tp2_Grupo7/src/main/java/ar/edu/unju/fi/repository/SucursalRepository.java
@@ -4,11 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import ar.edu.unju.fi.entity.Sucursal;
 
-@Repository
 public interface SucursalRepository extends JpaRepository<Sucursal, Long> {
 
     List<Sucursal> findByProvincia(String provincia);
